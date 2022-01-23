86f78ba8e7befba3a4de655ea80995e33f0e8bc8
LZHong <790795324@qq.com>
2022-01-23 23:20:24 +0800

    bugfix: 物资首页搜索后页数未置1 sprintfix:1.修改首页布局 2.修改首页翻页逻辑 3.修改首页点击搜索后出现的返回初始页面的布局以及样式

--- frontend/src/views/purchaseHome/index.vue
+++ frontend/src/views/purchaseHome/index.vue
@@ -6,10 +6,10 @@
                 </path>
             </svg>
         </div>
-        <bk-button :text="true" title="primary" @click="toIndex()" class="indexButton" v-if="showReturn">
-            <bk-icon type="angle-double-left" style="font-size: 20px;" /> 返回首页
-        </bk-button>
         <div class="search">
+            <div class="returnIndex" v-if="showReturn">
+                <bk-link theme="default" icon="bk-icon icon-angle-double-left" @click="toIndex" style="display: inline">返回初始页面</bk-link>
+            </div>
             <bk-input v-model="searchContent" :clearable="false" :font-size="'medium'" size="large">
                 <bk-dropdown-menu class="group-text" @show="dropdownShow" @hide="dropdownHide" ref="dropdown" slot="prepend" :font-size="'medium'">
                     <bk-button type="primary" slot="dropdown-trigger" class="selectType">
@@ -28,8 +28,8 @@
                 </template>
             </bk-input>
         </div>
-        <div class="banner" v-if="showBanner">
-            <bk-swiper :pics="bannerPics" height="300" width="900" :is-loop="true" class="swiper"></bk-swiper>
+        <div class="banner" v-if="!showReturn">
+            <bk-swiper :pics="bannerPics" height="270" width="900" :is-loop="true" class="swiper"></bk-swiper>
         </div>
         <div class="exception-wrap" v-if="showEmpty">
             <bk-exception class="exception-wrap-item exception-part" type="search-empty" scene="page" :class="{ 'exception-gray': isGray }"> </bk-exception>
@@ -37,8 +37,8 @@
         <div class="goods">
             <div class="goodCard" v-for="(item, index) in goodsList" :key="index" @click="toDetail(item.id)">
                 <bk-card :show-head="false" class="bkCard">
-                    <img :src="item.pics" alt="" style="width: 250px; height: 200px" v-if="item.pics">
-                    <img src="../../../static/images/nopic.png" alt="" style="width: 210px; height: 200px" v-if="!item.pics">
+                    <img :src="item.pics" alt="" style="width: 200px; height: 170px; margin: 10px 10px 0 10px" v-if="item.pics">
+                    <img src="../../../static/images/nopic.png" alt="" style="width: 200px; height: 180px; margin: 10px 10px 0 10px" v-if="!item.pics">
                     <p class="goodName">{{ item.good_name }}</p>
                     <div class="goodPrice">￥<div style="color: orange">{{ item.price }}</div></div>
                     <p>{{ item.cn_status }}</p>
@@ -83,6 +83,7 @@
                 isDropdownShow: false, // 种类组件参数
                 currentPage: 1, // 当前页数（不用于请求）
                 paramPage: 1, // 用于请求的页数
+                pageSize: 4,
                 pageCount: 0, // 总页数
                 config: { // 组件参数
                     strokeWidth: 10,
@@ -90,7 +91,6 @@
                     activeColor: '#3a84ff'
                 },
                 showReturn: 0, // 是否展示返回首页按钮
-                showBanner: 1, // 是否展示轮播图
                 showEmpty: 0,
                 goodInfo: {
                     num: 1,
@@ -133,15 +133,20 @@
                 })
             },
             getGoods () { // 获取指定商品信息
-                if (this.lastType !== this.type) {
-                    this.paramPage = 1
+                // if (this.lastType !== this.type) {
+                //     this.paramPage = 1
+                // }
+                if (this.showReturn === 0) {
+                    this.pageSize = 4
+                } else {
+                    this.pageSize = 8
                 }
                 this.$http.get(goodsUrl, {
                     params: {
                         good_name: this.searchContent, // 指定商品名称所含内容
                         good_type_id: this.type.id, // 指定商品类型
                         page: this.paramPage, // 指定页
-                        size: 8
+                        size: this.pageSize
                     }
                 }).then(res => {
                     if (res && res.result === true) {
@@ -149,18 +154,21 @@
                         this.lastSearchContent = this.searchContent
                         this.currentPage = this.paramPage
                         this.goodsList = res.data.good_list
-                        this.bannerPics = []
-                        for (let i = 0; i < res.data.good_list.length; i++) {
-                            if (i < this.bannerNum && this.currentPage === 1) { // 给轮播图列表赋值，逻辑是将前六个商品图片赋值给轮播图列表
-                                const bannerObject = Object()
-                                bannerObject.url = this.goodsList[i].pics[0] ? this.goodsList[i].pics[0] : '../../../static/images/nopic.png'
-                                bannerObject.link = detailUrl + '?goodId=' + this.goodsList[i].id
-                                this.bannerPics.push(bannerObject)
+                        if (this.bannerPics.length === 0) {
+                            for (let i = 0; i < res.data.good_list.length; i++) {
+                                if (i < this.bannerNum) { // 给轮播图列表赋值，逻辑是将前六个商品图片赋值给轮播图列表
+                                    const bannerObject = Object()
+                                    bannerObject.url = this.goodsList[i].pics[0] ? this.goodsList[i].pics[0] : '../../../static/images/nopic.png'
+                                    bannerObject.link = detailUrl + '?goodId=' + this.goodsList[i].id
+                                    this.bannerPics.push(bannerObject)
+                                }
                             }
+                        }
+                        for (let i = 0; i < res.data.good_list.length; i++) {
                             this.goodsList[i].pics = this.goodsList[i].pics[0] // 将商品的第一个图片作为展示图片
                         }
-                        this.pageCount = res.data.total_num / 8 * 10 // 给页面总数赋值
-                        if (res.data.total_list.length === 0) {
+                        this.pageCount = res.data.total_num / this.pageSize * 10 // 给页面总数赋值
+                        if (res.data.total_num === 0) {
                             this.showEmpty = 1 // 显示搜索为空
                         } else {
                             this.showEmpty = 0
@@ -209,7 +217,8 @@
             },
             search () { // 点击搜索触发事件
                 this.showReturn = 1
-                this.showBanner = 0
+                this.currentPage = 1
+                this.paramPage = this.currentPage
                 // TODO: 对type和searchContent进行post获取新数据
                 this.getGoods()
             },
@@ -225,6 +234,9 @@
 
 <style lang="postcss" scoped>
 .index-wrapper{
+    width: 90%;
+    margin: 0 auto;
+    overflow: auto;
     .toCarTab{
         position: absolute;
         bottom: 180px;
@@ -246,10 +258,6 @@
         path{
             fill: #3A84FF;
         }
-}
-    .indexButton{
-        margin: 20px 30px;
-        color: #000;
     }
     .banner{
         width: 100%;
@@ -271,10 +279,16 @@
         
     }
     .search{
-        margin: 30px;
+        margin: 10px auto;
         width: 80%;
         text-align: center;
         margin-left: 130px;
+        display: flex;
+        .returnIndex{
+            width: 120px;
+            line-height: 35px;
+            margin-right: 20px;
+        }
         .selectType{
             background-color: #f2f2f2;;
         }
@@ -284,34 +298,38 @@
     }
     .goods{
         height: auto;
-        width: 1480px;
-        margin: 50px auto 0 auto;
+        width: 1040px;
+        margin: 0 auto;
         overflow: hidden;
         .goodCard{
-            width: 290px;
+            width: 220px;
+            height: 270px;
             display: inline-block;
             float: left;
-            margin: 20px 40px;
+            margin: 0 20px;
+            /deep/ .bk-card-body {
+                width: 100%;
+                padding: 0;
+            }
             .goodName{
-                font-size: 25px;
+                font-size: 15px;
+                margin-left: 20px;
             }
             .goodPrice{
+                margin-top: 5px;
+                margin-left: 15px;
                 display: flex;
-                font-size: 15px;
-            }
-            .goodStatus{
-                color: #b0b0b0;
                 font-size: 10px;
             }
             .addButton{
                 text-align: right;
+                font-size: 5px;
+                margin: 0 10px 5px 0;
             }
             img{
                 width: 200px;
             }
             p {
-                margin-top: 10px;
-                margin-bottom: 10px;
                 &:last-child {
                     margin-bottom: 0;
                     color: #b2b1b1;
@@ -321,9 +339,9 @@
         }
     }
     .paginator{
+        margin-top: 5px;
         width: 100%;
         display: inline-block;
-        margin-top: 70px;
     }
 }
 </style>
