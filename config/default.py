192c69cdc332feb1d7cae0ba988fae19aac23d34
Jsummer121 <starsjiang@icloud.com>
2022-01-25 14:53:51 +0800

    feature: 添加管理员和修改环境变量

--- config/default.py
+++ config/default.py
@@ -110,7 +110,7 @@ LOGGING = get_logging_config_dict(locals())
 
 # 初始化管理员列表，列表中的人员将拥有预发布环境和正式环境的管理员权限
 # 注意：请在首次提测和上线前修改，之后的修改将不会生效
-INIT_SUPERUSER = []
+INIT_SUPERUSER = ["1789572985Q", "1271768177Q"]
 
 # 使用mako模板时，默认打开的过滤器：h(过滤html)
 MAKO_DEFAULT_FILTERS = ["h"]
@@ -172,4 +172,4 @@ if locals().get("DISABLED_APPS"):
 
 # 存放媒体数据的路径
 MEDIA_ROOT = os.path.join(BASE_DIR, "USERRES")
-BK_BACK_URL = os.getenv('BK_BACK_URL')
+BK_BACK_URL = os.getenv('BKAPP_BACK_URL')
