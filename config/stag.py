680ea8514b240cd5ee0aa1234381bcb5b529acea
LZHong <790795324@qq.com>
2022-03-15 15:04:11 +0800

    sprintfix: 修改数据库配置

--- config/stag.py
+++ config/stag.py
@@ -40,17 +40,28 @@ if FRONTEND_BACKEND_SEPARATION:
     CORS_ORIGIN_ALLOW_ALL = True
     CORS_ALLOW_CREDENTIALS = True
 
-DATABASES.update(
-    {
-        "default": {
-            "ENGINE": "django.db.backends.mysql",
-            "NAME": os.getenv("BKAPP_MYSQL_DBNAME"),  # 数据库名
-            "USER": os.getenv("BKAPP_MYSQL_USER"),  # 数据库用户
-            "PASSWORD": os.getenv("BKAPP_MYSQL_PASSWORD"),  # 数据库密码
-            "HOST": os.getenv("BKAPP_MYSQL_HOST"),  # 数据库主机
-            "PORT": os.getenv("BKAPP_MYSQL_PORT"),  # 数据库端口
-        },
+# DATABASES.update(
+#     {
+#         "default": {
+#             "ENGINE": "django.db.backends.mysql",
+#             "NAME": os.getenv("BKAPP_MYSQL_DBNAME"),  # 数据库名
+#             "USER": os.getenv("BKAPP_MYSQL_USER"),  # 数据库用户
+#             "PASSWORD": os.getenv("BKAPP_MYSQL_PASSWORD"),  # 数据库密码
+#             "HOST": os.getenv("BKAPP_MYSQL_HOST"),  # 数据库主机
+#             "PORT": os.getenv("BKAPP_MYSQL_PORT"),  # 数据库端口
+#         },
+#     }
+# )
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_NAME'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOST'),
+        'PORT': os.environ.get('MYSQL_PORT'),
     }
-)
+}
 
 MEDIA_URL = '%smedia/' % SITE_URL
