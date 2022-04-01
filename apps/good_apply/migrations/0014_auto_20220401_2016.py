76b6db6567c7ba580bd4971115ea884e32ca6bd0
LZHong <790795324@qq.com>
2022-04-01 20:49:36 +0800

    sprintfix:1. 优化迁移文件

--- apps/good_apply/migrations/0014_auto_20220401_2016.py
+++ apps/good_apply/migrations/0014_auto_20220401_2016.py
@@ -5,8 +5,8 @@ from django.db import migrations
 
 def add_new_organizaition(apps, schema_editor):
     Organization = apps.get_model('good_apply', 'Organization')
-    obj = Organization(id=10001, group_name='GoodApply')
-    obj.save()
+    obj = Organization.objects.create(id=1000000, group_name='GoodApply')
+    obj.delete()
 
 
 class Migration(migrations.Migration):
